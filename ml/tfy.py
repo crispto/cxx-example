f145089b68e89ef2e7a514a39c7d8000e5a4e174
crispto <2273713147@qq.com>
2022-05-23 00:06:53 +0800

    test ml

--- ml/tfy.py
+++ ml/tfy.py
@@ -18,6 +18,8 @@ model = tf.keras.models.Sequential([
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
+
+
 model.fit(x_train, y_train, epochs=5)
 
 model.evaluate(x_test,  y_test, verbose=2)
