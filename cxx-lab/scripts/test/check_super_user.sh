89d52763e6bb03457ff40e41775c3f0283602d72
crispto <2273713147@qq.com>
2022-05-25 17:12:47 +0800

    refactor

--- cxx-lab/scripts/test/check_super_user.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-if [ $UID -ne 0 ]; then
-  echo 'you are not super use'
-else
-  echo 'you are super user'
-fi
--- cxx-lab/scripts/test/word_count_dict.sh
+++ cxx-lab/scripts/test/word_count_dict.sh
@@ -1,21 +1,21 @@
-declare -a dic
+declare -A dic
 dic=()
-
-
-
-
 process(){
   echo $1;
 }
-a="m w m"
+a="m w m a x x a w m w m m"
 
 read -r -a array <<< $a;
 for word in ${array[@]}
 do
-  if [ -n "${dic[$word]}" ];then
+  if [ "$word" != 'm' -a "$word" != 'w' ];then
+    echo "get extra word " $word
+    continue;
+  elif [ -n "${dic[$word]}" ];then
     dic["$word"]=`expr ${dic[$word]} + 1`
   else
     dic["$word"]=1
+
   fi
 done
 
