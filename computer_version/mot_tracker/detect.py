d2cf2c06c26e7bbbaab5aecf2f4c18642b501c63
crispto <2273713147@qq.com>
2022-06-06 00:01:45 +0800

    yolov5

---
+++ computer_version/mot_tracker/detect.py
@@ -0,0 +1,6 @@
+import cv2
+
+
+def main():
+    path = "/home/richard/test-database/mot/sample1.mp4"
+    data = cv2.Camera
--- computer_version/opencv_test.py
+++ computer_version/opencv_test.py
@@ -1,4 +1,5 @@
 import os
 import stat
 
+os.makeup()
 os.chown("./dave.jpg", 200, 200)
