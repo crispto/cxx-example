0216dd00e706a252517c83f2a501875ed18e80ba
yangchengwei <yangchengwei@senseauto.com>
2022-07-28 23:04:51 +0800

    add opencv test

---
+++ computer_version/opencv-cxx/src/read-pic.cpp
@@ -0,0 +1,9 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+int main(){
+  cv::Mat image; // empty image
+  std::cout << "width " << image.rows << " ,height: " << image.cols << std::endl;
+
+}
