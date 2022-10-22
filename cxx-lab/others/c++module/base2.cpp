e185c926ebbc0c5a2fb40812cf22ef287af21539
crispto <2273713147@qq.com>
2022-10-22 15:31:19 +0800

    cpp class memory space

---
+++ cxx-lab/others/c++module/base2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+//继承， 不含多态
+class Point2D{
+  public:
+  int x;
+  short y;
+  void call(){
+    cout << "2d call" << endl;
+  };
+  void only2d(){
+    cout << "2d only called " << endl;
+
+  }
+};
+
+class Painter{
+  public:
+  void call(){
+    cout << "painter call " << endl;
+  }
+};
+
+class Point3D: public Point2D, public Painter{
+  public:
+  int z;
+};
+
+class Point3Dshort: public Point2D{
+  public:
+  short z;
+};
+
+int main(){
+  cout << "size of point2d: " << sizeof(Point2D) << endl;
+  cout << "size of point3d: " << sizeof(Point3D) << endl;
+  cout << "size of point3dshort: " << sizeof(Point3Dshort) << endl;
+
+  Point3D p;
+  p.Point2D::call(); // 多继承 父类函数重名的情况下需要在调用时精确指定哪个父类
+  p.Painter::call();
+  p.only2d(); // 这里会发生强制转换，Point3D 会被转换成  Point2D
+  return 0;
+
+}
