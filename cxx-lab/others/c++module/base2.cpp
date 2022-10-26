0ff41c29f7e14f7cd8096297f0edbc68c82a2606
crispto <2273713147@qq.com>
2022-10-27 00:14:37 +0800

    overwrite

--- cxx-lab/others/c++module/base2.cpp
+++ cxx-lab/others/c++module/base2.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 //继承， 不含多态
 class Point2D{
-  public:
+  private:
   int x;
   short y;
-  void call(){
+  public:
+  virtual void call(){
     cout << "2d call" << endl;
   };
   void only2d(){
@@ -21,9 +22,12 @@ class Painter{
   }
 };
 
-class Point3D: public Point2D, public Painter{
+class Point3D: public Point2D{
   public:
   int z;
+  void call() override{
+    std::cout << "3d calle" << std::endl;
+  }
 };
 
 class Point3Dshort: public Point2D{
@@ -36,9 +40,11 @@ int main(){
   cout << "size of point3d: " << sizeof(Point3D) << endl;
   cout << "size of point3dshort: " << sizeof(Point3Dshort) << endl;
 
-  Point3D p;
+  Point3D p0;
+  Point3D & p = p0;
+  p.call();
   p.Point2D::call(); // 多继承 父类函数重名的情况下需要在调用时精确指定哪个父类
-  p.Painter::call();
+  // p.Painter::call();
   p.only2d(); // 这里会发生强制转换，Point3D 会被转换成  Point2D
   return 0;
 
