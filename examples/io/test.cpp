5320f08b91c6ba7d21333bed9e419153fd0b0e5c
yangchengwei <yangchengwei@senseauto.com>
2023-11-10 20:26:43 +0800

    feature: add Eigen and Cmd Parser

--- examples/io/test.cpp
+++ examples/io/test.cpp
@@ -2,9 +2,23 @@
 #include <iterator>
 #include <sstream>
 #include <vector>
+using namespace std;
 
+void test_array()
+{
+    int tmp[20]{ 0 };
+    for (int i = 0; i < 20; i++) {
+        std::cout << tmp[i];
+        if (i == 19) {
+            cout << endl;
+        } else {
+            cout << " ";
+        }
+    }
+}
 int main()
 {
+    test_array();
     std::string text = "Let me split this into words";
 
     std::istringstream iss(text);
