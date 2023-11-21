19e7e515cf69077d04c2ddf0aaa9633fb5b8794c
yangchengwei <yangchengwei@senseauto.com>
2023-11-21 21:01:06 +0800

    add tensort demo

--- libtest/libtorch/tensorrt/basic.cpp
+++ libtest/libtorch/tensorrt/basic.cpp
@@ -1,5 +1,19 @@
+#include <co/fs.h>
 #include <iostream>
+/**
+ * @brief 使用tensorrt加速模型推理
+ *
+ * @return int
+ */
+using namespace co;
 
 int main()
 {
+    fastring filepath = "model/yolov8n.trt";
+    if (!fs::exists(filepath)) {
+        std::cout << "file not exists: " << filepath << std::endl;
+        return 1;
+    }
+    size_t sz = fs::fsize(filepath);
+    std::cout << "file size: " << sz << std::endl;
 }
